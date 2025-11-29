9d97dd6b4e167ca098e0eb5ca54705275d7da60f
DESKTOP-V48IMQF\DELL <174677713@qq.com>
2025-11-29 17:25:56 +0800

    ring

--- src/views/load.vue
+++ src/views/load.vue
@@ -1,38 +1,24 @@
 <template>
   <div class="container">
-    <div>test</div>
-
-    <div class="memory-info">
-      内存使用: {{ memoryUsage.used }}MB / {{ memoryUsage.total }}GB
+    <div class="loading-wrapper">
+      <div class="loading-bg">
+        <img src="@/assets/pic/device.png" alt="" />
+      </div>
+      <svg class="progress-ring" width="320" height="320">
+        <circle
+          cx="160"
+          cy="160"
+          r="155"
+          stroke="#c293d5"
+          stroke-width="10"
+          fill="none"
+          stroke-linecap="round"
+          class="progress-circle"
+        />
+      </svg>
     </div>
-    <div>test</div>
   </div>
 </template>
-
-<script setup>
-import { ref, onMounted, onUnmounted } from "vue";
-
-// 初始化内存数据
-const memoryUsage = ref({ used: "0", total: "0" });
-let removeMemoryListener = null;
-
-onMounted(() => {
-  if (window.electronAPI) {
-    removeMemoryListener = window.electronAPI.onMemoryUsage((data) => {
-      if (data && typeof data === "object") {
-        memoryUsage.value = {
-          used: data.used || "0",
-          total: data.total || "0",
-        };
-      }
-    });
-  }
-});
-
-onUnmounted(() => {
-  if (removeMemoryListener) removeMemoryListener();
-});
-</script>
 
 <style scoped lang="scss">
 .container {
@@ -40,14 +26,55 @@ onUnmounted(() => {
   width: 100%;
   height: 100vh;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   box-sizing: border-box;
-  font-size: 40px;
-  color: red;
-  .memory-info {
-    font-size: 40px;
-    color: red;
+
+  .loading-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: relative;
+
+    .loading-bg {
+      width: 400px;
+      height: 400px;
+      background: url("@/assets/pic/backgroundImage.png");
+      border-radius: 50%;
+      overflow: hidden;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      display: flex;
+      position: relative;
+      z-index: 2;
+
+      img {
+        width: 300px;
+        height: 300px;
+      }
+    }
+
+    .progress-ring {
+      position: absolute;
+      .progress-circle {
+        stroke-dasharray: 1288; /* 2 * π * 205 */
+        stroke-dashoffset: 1288;
+        animation: progressAnimation 3s linear forwards;
+        transform-origin: center;
+        transform: rotate(-90deg);
+      }
+    }
+  }
+}
+
+@keyframes progressAnimation {
+  0% {
+    stroke-dashoffset: 1288;
+  }
+  100% {
+    stroke-dashoffset: 0;
   }
 }
 </style>
