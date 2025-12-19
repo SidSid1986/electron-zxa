85a9cc87f42fde96823e05c151fbaeac56297ff4
DESKTOP-V48IMQF\DELL <174677713@qq.com>
2025-12-19 16:28:30 +0800

    table ready for com

--- vite.config.js
+++ vite.config.js
@@ -49,19 +49,19 @@ export default ({ mode }) => {
         gifsicle: { optimizationLevel: 3 },
         pngquant: { quality: [0.6, 0.8] },
       }),
-      // ✅ 关键：Element Plus 插件强制使用源码，关闭自动样式注入
+      //  Element Plus 插件强制使用源码，关闭自动样式注入
       ElementPlus({
         useSource: true,
         // 关闭自动导入样式，交给 main.js 全局引入
         importStyle: false,
       }),
-      // ✅ 自动导入仅导入 API，不导入样式
+      //  自动导入仅导入 API，不导入样式
       AutoImport({
         resolvers: [ElementPlusResolver({ importStyle: false })],
         imports: ["vue", "vue-router", "pinia"],
         dts: false,
       }),
-      // ✅ 组件自动导入仅注册组件，不导入样式
+      //  组件自动导入仅注册组件，不导入样式
       Components({
         resolvers: [ElementPlusResolver({ importStyle: false })],
         dts: false,
